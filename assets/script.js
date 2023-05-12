705f9162c2a3ecf2313d92eb2058fcb3d07afa8f
dsoda86 <122141740+dsoda86@users.noreply.github.com>
2023-05-11 17:33:21 -0700

    Created variables for all of my ids using getElementById instead of jquery so I can more easily follow current activity code examples which don't use it.

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,14 @@
  var apiKey = "aa6a965bdeca3df25b15a8693bbf86d9";
  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}";
 
+ var searchCity = document.getElementById("#search-city");
+ var cityInputEl = document.getElementById("#city-input");
+ var searchBtn = document.getElementById("#search-button")
+ var clearBtn = document.getElementById("#clear-button");
+ var pastCitiesEl = document.getElementById("#past-cities");
+ var currentWeather = document.getElementById("#current-weather");
+ var futureForecast = document.getElementById("#future-forecast");
+ var searchHistory = [];
 
 //Needs:
 //event trigger for clicking on search button / formSubmitHandler from activity?
